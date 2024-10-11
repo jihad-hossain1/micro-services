dc667f1e477c09db7263c48cdd3e80a75c3395bf
jihad-hossain1 <jihadkhan934@gmail.com>
2024-10-11 12:55:56 +0600

    api getway added

---
+++ api-getway/src/utils.ts
@@ -0,0 +1,50 @@
+import { Express, Request, Response, NextFunction } from "express";
+import config from "./config.json";
+import axios from "axios";
+
+export const createHandler = (
+    hostName: string,
+    path: string,
+    method: string,
+) => {
+    return async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            let url = `${hostName}${path}`;
+
+            req.params &&
+                Object.keys(req.params).forEach((param) => {
+                    url = url.replace(`:${param}`, req.params[param]);
+                });
+
+            const { data } = await axios({
+                method,
+                url,
+                data: req.body,
+            });
+
+            res.status(200).json(data);
+        } catch (error) {
+            if (error instanceof axios.AxiosError) {
+                return res.status(error.response?.status || 500).json({
+                    error: error.response?.data || error.message,
+                });
+            }
+            return res.status(500).json({
+                error: (error as Error).message,
+                message: "Internal server error",
+            });
+        }
+    };
+};
+
+export const configureRoutes = (app: Express) => {
+    Object.entries(config.services).forEach(([name, service]) => {
+        const hostName = service.url;
+        service.routes.forEach((route) => {
+            route.methods.forEach((method) => {
+                const handler = createHandler(hostName, route.path, method);
+                app[method](`/api${route.path}`, handler);
+            });
+        });
+    });
+};
